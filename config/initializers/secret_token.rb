d2735af34fc1e0c9949e74f6e1a6cc0b1ac70254
Makeam <miel.design@gmail.com>
2013-12-08 12:47:33 +0400

    Пустой проект с регистрацией

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mailer::Application.config.secret_key_base = '88de217f566665e79421e1e70fa4c72812e7671ec31424cf70715ad293ae01f4d122509312032f633e1b02fb0f1e6fb2b709cb5b58b0be848dec9e14333d0931'
