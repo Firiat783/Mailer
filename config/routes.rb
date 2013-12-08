9798cbfc9179665bb73982e55c51eb08bd0fed04
m19804 <m19804@yandex.ru>
2013-12-08 13:12:56 +0400

    добавил контроллер для юзеров

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Mailer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
+   get "people/index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
