7b737467dd51d6b98220999aab9391140aab1e59
m19804 <m19804@yandex.ru>
2013-12-08 15:40:03 +0400

    Resolved conflict

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,14 @@ class MessagesController < ApplicationController
     @message.receiver_id = params[:receiver_id]
   end
 
-<<<<<<< HEAD
   def create
     @message = Message.new
 
   end
 
+  def index
+    @messages=Message.where(user_id:current_user.id)
+  end
 
 private
 
@@ -18,10 +20,5 @@ private
       params[:message].permit(:receiver_id, :content, :title)
     end
 
-=======
-  def index
-     @messages=Message.where(user_id:current_user.id)
-  end
 
->>>>>>> 272080b000f6aeca71367551e7e3d2192f03621f
 end
